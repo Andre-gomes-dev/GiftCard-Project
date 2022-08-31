bd27981544c23627916942e1a150fecbe906fc67
andre-gomes <gomesandre749@gmail.com>
2022-08-31 10:32:29 -0300

    começo do projeto

---
+++ src/test/java/br/com/giftproject/GiftProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giftproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
