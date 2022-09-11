81d028b82901694d95dbeee89ea72552c2e14638
andre-gomes <gomesandre749@gmail.com>
2022-09-10 23:28:25 -0300

    "adcionando retorno dos tipos de GiftCard com @get. todos são enums"

--- src/main/java/br/com/giftproject/controller/CarrinhoComprasController.java
+++ src/main/java/br/com/giftproject/controller/CarrinhoComprasController.java
@@ -1,15 +1,23 @@
 package br.com.giftproject.controller;
 
+import br.com.giftproject.model.TipoGift;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/carrinhocompra")
 public class CarrinhoComprasController {
 
-
-
-
+        @GetMapping("/devolvetipos")
+            public List<TipoGift> devolveTiposGift() {
+            return new ArrayList<>(Arrays.asList(TipoGift.values()));
+        }
 //    @PostMapping()
 //    public ResponseEntity<GiftController>criandoCarrinho(){
 //
